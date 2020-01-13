d09415206b25dabb469017e1e1b7e90c6ef24346
Iryna <iryna.malyshevska@rule.se>
2020-01-13 14:00:54 +0200

    update version

--- woorule.php
+++ woorule.php
@@ -8,7 +8,7 @@
  * Plugin Name:     WooRule
  * Plugin URI:      http://github.com/rulecom/woorule
  * Description:     RuleMailer integration for WooCommerce.
- * Version:         0.5
+ * Version:         0.6
  * Author:          RuleMailer, Neevalex
  * Author URI:      http://rule.se
  * Developer:       Jonas Adolfsson, Neev Alex
