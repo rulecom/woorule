a050fc00f804eb4d816d6681e46aa1b27d602161
webcodist <me@webcodist.com>
2022-01-21 15:37:52 +0300

    Implemented cart update events handling.

--- inc/class-woorule.php
+++ inc/class-woorule.php
@@ -7,6 +7,7 @@
 
 require_once WOORULE_PATH . 'inc/class-woorule-checkout.php';
 require_once WOORULE_PATH . 'inc/class-woorule-order-hooks.php';
+require_once WOORULE_PATH . 'inc/class-woorule-cart-hooks.php';
 require_once WOORULE_PATH . 'inc/class-woorule-shortcode.php';
 require_once WOORULE_PATH . 'inc/class-woorule-options.php';
 require_once WOORULE_PATH . 'inc/class-rulemailer-api.php';
@@ -26,6 +27,7 @@ class Woorule {
 		if ( $this->is_woocommerce_activated() ) {
 			new Woorule_Checkout();
 			new Woorule_Order_Hooks();
+			new Woorule_Cart_Hooks();
 			new Woorule_Shortcode();
 		} else {
 			add_action( 'admin_notices', array( $this, 'notice_woo_error' ) );
