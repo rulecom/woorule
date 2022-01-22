1ba09df886363e0d9440ef26a95f3ff6f6f3e170
webcodist <me@webcodist.com>
2022-01-22 19:22:07 +0300

    Preparation for integration implementation.

--- inc/class-woorule-options.php
+++ inc/class-woorule-options.php
@@ -5,80 +5,165 @@
  * @package Woorule
  */
 
+// phpcs:disable Squiz.Commenting.FunctionComment.SpacingAfterParamType
+
 /**
  * Class Woorule_Options
+ *
+ * @method static get_api_key()
+ * @method static get_checkout_tags()
+ * @method static get_checkout_label()
+ * @method static get_checkout_show()
+ * @method static get_options()
+ * @method static set_api_key( string $value )
+ * @method static set_checkout_tags( string $value )
+ * @method static set_checkout_label( string $value )
+ * @method static set_checkout_show( string $value )
+ * @method static set_options( array $options )
  *
  * @package Woorule
  */
 class Woorule_Options {
+	const OPTIONS_KEY = 'woocommerce_rulemailer_settings';
+
 	/**
 	 * Plugin options.
 	 *
 	 * @var array
 	 */
-	protected static $options = array();
+	protected $options = array();
 
 	/**
-	 * Get API key.
-	 *
-	 * @return string
-	 */
-	public static function get_api_key() {
-		return self::get_option( 'woorule_api_key' );
-	}
-
-	/**
-	 * Get checkout tags.
-	 *
-	 * @return string
-	 */
-	public static function get_checkout_tags() {
-		return self::get_option( 'woorule_checkout_tags' );
-	}
-
-	/**
-	 * Get checkout label.
-	 *
-	 * @return string
-	 */
-	public static function get_checkout_label() {
-		return self::get_option( 'woorule_checkout_label' );
-	}
-
-	/**
-	 * Get checkout show.
-	 *
-	 * @return string
-	 */
-	public static function get_checkout_show() {
-		return self::get_option( 'woorule_checkout_show' );
-	}
-
-	/**
-	 * Get option.
-	 *
-	 * @param string $option_name Option name.
-	 *
-	 * @return string
-	 */
-	protected static function get_option( $option_name ) {
-		if ( empty( self::$options ) ) {
-			self::$options = get_option( 'woocommerce_rulemailer_settings', array() );
-		}
-
-		return isset( self::$options[ $option_name ] ) ? self::$options[ $option_name ] : '';
-	}
-
-	/**
-	 * Set plugin options.
-	 *
-	 * @param array $options Options.
+	 * Woorule_Options constructor.
 	 *
 	 * @return void
 	 */
-	public static function set_options( $options ) {
-		self::$options = wp_parse_args(
-			$options,
+	protected function __construct() {
+		$this->options = get_option( self::OPTIONS_KEY, array() );
+	}
+
+	/**
+	 * Prevent unserializing.
+	 *
+	 * @return void
+	 */
+	protected function __wakeup() {
+	}
+
+	/**
+	 * Prevent cloning.
+	 *
+	 * @return void
+	 */
+	protected function __clone() {
+	}
+
+	/**
+	 * Get class instance.
+	 *
+	 * @return object Instance.
+	 */
+	protected static function get_instance() {
+		static $instance = null;
+
+		if ( is_null( $instance ) ) {
+			$instance = new static();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Get option value magic method.
+	 *
+	 * @param string $name Getter functions name.
+	 * @param array $arguments Functions arguments.
+	 *
+	 * @return mixed
+	 *
+	 * @throws BadMethodCallException Exception if not a getter function.
+	 */
+	public static function __callStatic( $name, $arguments ) {
+		$instance = self::get_instance();
+
+		$instance->filter_options();
+
+		if ( 0 === strpos( $name, 'get_' ) ) {
+			return $instance->get( substr( $name, 4 ) );
+		} elseif ( 0 === strpos( $name, 'set_' ) ) {
+			$instance->set( substr( $name, 4 ), $arguments[0] );
+		} else {
+			throw new BadMethodCallException( $name . ' is not defined in ' . __CLASS__ );
+		}
+
+		return null;
+	}
+
+	/**
+	 * Implement getter functions.
+	 *
+	 * @param string $option_name Option name.
+	 *
+	 * @return mixed
+	 *
+	 * @throws BadMethodCallException Exception.
+	 */
+	protected function get( $option_name ) {
+		if ( 'options' === $option_name ) {
+			return $this->options;
+		} elseif ( isset( $this->options[ 'woorule_' . $option_name ] ) ) {
+			return $this->options[ 'woorule_' . $option_name ];
+		} else {
+			throw new BadMethodCallException( $option_name . ' is not defined in ' . __CLASS__ );
+		}
+	}
+
+	/**
+	 * Implement setter functions.
+	 *
+	 * @param string $option_name Option name.
+	 * @param mixed $value Value.
+	 *
+	 * @return void
+	 *
+	 * @throws BadMethodCallException Exception.
+	 */
+	protected function set( $option_name, $value ) {
+		static $updated = null;
+
+		if ( 'options' === $option_name && is_array( $value ) ) {
+			// Merge new options with existent object's options.
+			$value = wp_parse_args( $value, $this->options );
+			$this->filter_options( $value );
+		} elseif ( isset( $this->options[ 'woorule_' . $option_name ] ) ) {
+			$this->options[ 'woorule_' . $option_name ] = $value;
+		} else {
+			throw new BadMethodCallException( $option_name . ' is not defined in ' . __CLASS__ );
+		}
+
+		if ( is_null( $updated ) ) {
+			$updated = true;
+
+			// Save options to DB on shutdown.
+			add_action(
+				'shutdown',
+				// Anonymous function is used because we do want to have public DB update function.
+				function () {
+					$this->filter_options();
+					update_option( self::OPTIONS_KEY, $this->options, false );
+				}
+			);
+		}
+	}
+
+	/**
+	 * Get options defaults.
+	 *
+	 * @return array
+	 */
+	protected function get_options_defaults() {
+		return (array) apply_filters(
+			'woorule_options_defaults',
 			array(
 				'woorule_api_key'        => '',
 				'woorule_checkout_tags'  => '',
@@ -86,7 +171,19 @@ class Woorule_Options {
 				'woorule_checkout_show'  => '',
 			)
 		);
+	}
 
-		update_option( 'woocommerce_rulemailer_settings', self::$options, false );
+	/**
+	 * Filter options.
+	 *
+	 * @param array $options Options.
+	 *
+	 * @return void
+	 */
+	protected function filter_options( $options = null ) {
+		$this->options = shortcode_atts(
+			self::get_options_defaults(),
+			is_null( $options ) ? $this->options : $options
+		);
 	}
 }
