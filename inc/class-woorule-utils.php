5392c4948d9d1a998491c56543f362401f6a4914
Oleg Iskusnyh <oleg@iskusnyh.pro>
2022-02-24 18:59:26 +0600

    Fix intents

--- inc/class-woorule-utils.php
+++ inc/class-woorule-utils.php
@@ -22,39 +22,39 @@ class Woorule_Utils {
 		return number_format( (float) $value, wc_get_price_decimals(), '.', '' );
 	}
 
-    /**
-     * Get and format phone number.
-     *
-     * @param WC_Order $order
-     * @return string
-     */
-    public static function get_order_phone_number( WC_Order $order ) {
-        $phone = preg_replace( '/[^0-9\+]/', '', $order->get_billing_phone() );
+	/**
+	 * Get and format phone number.
+	 *
+	 * @param WC_Order $order
+	 * @return string
+	 */
+	public static function get_order_phone_number( WC_Order $order ) {
+		$phone = preg_replace( '/[^0-9\+]/', '', $order->get_billing_phone() );
 
-        if ( '+' !== substr( $phone, 0, 1 ) ) {
-            $code = WC()->countries->get_country_calling_code( $order->get_billing_country() );
+		if ( '+' !== substr( $phone, 0, 1 ) ) {
+			$code = WC()->countries->get_country_calling_code( $order->get_billing_country() );
 
-            return '+' . $code . $phone;
-        }
+			return '+' . $code . $phone;
+		}
 
-        return $phone;
-    }
+		return $phone;
+	}
 
-    /**
-     * Get and format customer's phone number.
-     *
-     * @param WC_Customer $customer
-     * @return string
-     */
-    public static function get_customer_phone_number( WC_Customer $customer ) {
-        $phone = preg_replace( '/[^0-9\+]/', '', $customer->get_billing_phone() );
+	/**
+	 * Get and format customer's phone number.
+	 *
+	 * @param WC_Customer $customer
+	 * @return string
+	 */
+	public static function get_customer_phone_number( WC_Customer $customer ) {
+		$phone = preg_replace( '/[^0-9\+]/', '', $customer->get_billing_phone() );
 
-        if ( '+' !== substr( $phone, 0, 1 ) ) {
-            $code = WC()->countries->get_country_calling_code( $customer->get_billing_country() );
+		if ( '+' !== substr( $phone, 0, 1 ) ) {
+			$code = WC()->countries->get_country_calling_code( $customer->get_billing_country() );
 
-            return '+' . $code . $phone;
-        }
+			return '+' . $code . $phone;
+		}
 
-        return $phone;
-    }
+		return $phone;
+	}
 }
