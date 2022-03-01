5f75dfbb967a6668169fafc138ca6d23e780945b
Oleg Iskusnyh <oleg@iskusnyh.pro>
2022-03-01 17:53:51 +0600

    Add testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
+}
+
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', __DIR__ . '/../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php' );
+if ( ! file_exists( WP_TESTS_PHPUNIT_POLYFILLS_PATH ) ) {
+	echo "Could not find " . WP_TESTS_PHPUNIT_POLYFILLS_PATH . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+$woocommerce_dir = getenv( 'WOOCOMMERCE_DIR' );
+if ( ! file_exists( $woocommerce_dir ) ) {
+	echo "Could not find $woocommerce_dir" . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
+}
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( __FILE__ ) ) . '/woorule.php';
+}
+
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $woocommerce_dir . '/tests/legacy/bootstrap.php';
