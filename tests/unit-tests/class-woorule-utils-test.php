6e9e3b4a87316962f0d7d7160eed13574b866676
Oleg Iskusnyh <oleg@iskusnyh.pro>
2022-03-02 10:35:55 +0600

    Add more tests

--- tests/unit-tests/class-woorule-utils-test.php
+++ tests/unit-tests/class-woorule-utils-test.php
@@ -1,12 +1,50 @@
 <?php
 
 class WC_Rule_Utils_Test extends WC_Unit_Test_Case {
-	public function test_payment_gateway() {
+	public function test_round() {
 		$object = new Woorule_Utils();
 		$this->assertInstanceOf( Woorule_Utils::class, new $object );
 
 		$result = Woorule_Utils::round( 125 );
 		$this->assertEquals( '125.00', $result );
 		$this->assertIsString( $result );
+	}
+
+	public function test_order_phone_number() {
+		$object = new Woorule_Utils();
+		$this->assertInstanceOf( Woorule_Utils::class, new $object );
+
+		/** @var WC_Order $order */
+		$order = WC_Helper_Order::create_order();
+		$order->set_billing_country( 'SE' );
+		$order->set_billing_phone( '732528523' );
+		$order->save();
+
+		$result = Woorule_Utils::get_order_phone_number( $order );
+		$this->assertEquals( '+46732528523', $result );
+
+		$order->set_billing_country( 'SE' );
+		$order->set_billing_phone( '+46732528523' );
+		$order->save();
+
+		$result = Woorule_Utils::get_order_phone_number( $order );
+		$this->assertEquals( '+46732528523', $result );
+
+		$order->set_billing_country( 'SE' );
+		$order->set_billing_phone( '46732528523' );
+		$order->save();
+
+		$result = Woorule_Utils::get_order_phone_number( $order );
+		$this->assertEquals( '+46732528523', $result );
+	}
+
+	public function test_customer_phone_number() {
+		/** @var WC_Customer $customer */
+		$customer = WC_Helper_Customer::create_mock_customer();
+		$customer->set_billing_country( 'SE' );
+		$customer->set_billing_phone( '732528523' );
+
+		$result = Woorule_Utils::get_customer_phone_number( $customer );
+		$this->assertEquals( '+46732528523', $result );
 	}
 }
